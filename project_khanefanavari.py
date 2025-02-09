8a59237fadf59fc986e88c6b516126455c41e77a
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-02-09 21:47:32 +0100

    Update project_khanefanavari.py

--- project_khanefanavari.py
+++ project_khanefanavari.py
@@ -1,3 +1,10 @@
+
+#--------------IMPORTS---------------------
+import numpy as np
+
+
+#--------------------DEVICE--------------------
+
 class Device:
     def __init__(self,topic,pin=None):
         self.topic=topic
@@ -16,7 +23,10 @@ class Device:
         #print('now it is turned off')
     def get_status(self):
         return self.status
-import numpy as np
+
+
+#--------------------SENSORS--------------------
+
 class Sensor:
     def __init__(self,name,group,unit,pin=None):
         self.name=name
@@ -27,21 +37,31 @@ class Sensor:
     def read_sensor(self):
         return np.random.uniform(20,25)
 
+
+
+#--------------------ADMIN PANEL--------------------
+
+
 class admin_panel:
     def __init__(self):
         self.groups={}
+        
     def create_group(self,group_name):
         if group_name not in self.groups:
             self.groups[group_name]=[]
             print(f'group {group_name} is created')
         else:
             print('your name is dublicated')
+
+    
     def add_device_to_group(self,group_name,device):
         if group_name in self.groups:
             self.groups[group_name].append(device)
             print(f'device {device} added in group {group_name}')
         else:
             print('f group {group_name} does not exsist')
+
+    
     def create_device(self,group_name,device_type,name):
         if group_name in self.groups:
             topic=f'home/{group_name}/{device_type}/{name}'
@@ -50,6 +70,8 @@ class admin_panel:
             print(f'device {device_type} craeted in group {group_name}')
         else:
             print('your group does not exist')
+
+    
     def create_multiple_devices(self,group_name,device_type,number_of_devcies):
         if group_name in self.groups:
             for i in range(1,number_of_devcies+1):
@@ -60,22 +82,30 @@ class admin_panel:
             print(f'{number_of_devcies} device {device_type} added to group {group_name}')
         else:
             print(f'group {group_name} does not exist')
+
+    
     def get_devices_in_groups(self,group_name):
         if group_name in self.groups:
             return self.groups[group_name]
             print(f'all devises in {group_name} ')
         else:
             print(f'group {group_name} does not exist')
+
+    
     def turn_on_all_in_groups(self,group_name):
         devices=self.get_devices_in_groups(group_name) 
         for device in devices:
             device.turn_on()
         print(f'all devices in group {group_name} are now turned on')
+
+    
     def turn_off_all_in_groups(self,group_name):
         devises=self.get_devices_in_groups(group_name)
         for devise in devises:
             devise.turn_off()
         print(f'all devices in group {group_name} are now turned off')
+
+    
     def turn_on_all_devices(self):
        for devices in self.groups.values():
           all_device=[]
@@ -83,6 +113,8 @@ class admin_panel:
           for i in all_device:
                i.turn_on()
        print('all devices are turned on now')
+
+    
     def turn_off_all_devices(self):
         for devices in self.groups.values():
             all_device=[]
@@ -90,6 +122,8 @@ class admin_panel:
         for i in all_device:
             i.turn_off()
         print('all devices are turned off now')
+
+    
     def get_status_in_group(self,gorup_name):
         if gorup_name in self.groups:
             all_device=self.get_devices_in_groups(gorup_name)
@@ -100,6 +134,8 @@ class admin_panel:
                     print(f'device {i.name} is off')
         else:
             print(f'group {gorup_name} does not exsist')
+
+    
     def get_status_in_device_type(self,device_type):
         all_device=[]
         for i in self.groups.values():
@@ -113,6 +149,7 @@ class admin_panel:
           else:
             print(f'{device_type} not found')
             break
+              
     def create_sensor(self,name,group,unit):
          if group in self.groups:
             topic=f'{name}/{group}/{unit}'
