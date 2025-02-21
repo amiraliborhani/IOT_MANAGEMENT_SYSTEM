4d00f65b6eeebcb93e0c726c150a1f1f1449c968
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-02-21 11:27:30 +0100

    Update project_khanefanavari.py

--- project_khanefanavari.py
+++ project_khanefanavari.py
@@ -4,6 +4,12 @@ APM: ahsant besiar awli
 tahe codeton yek example usage bezarid yani benevisid k chijori msieh estefade kard az class ha
 '''
 #salam aghaye pilevar add kardam
+
+'''
+
+APM: besiar awli check shod
+
+'''
 #=============================DEVCIE============================
 import numpy as np
 class Device:
@@ -54,12 +60,14 @@ class admin_panel:
             print(f'group {group_name} is created')
         else:
             print('your name is dublicated')
+            
     def add_device_to_group(self,group_name,device):
         if group_name in self.groups:
             self.groups[group_name].append(device)
             print(f'device {device} added in group {group_name}')
         else:
             print('f group {group_name} does not exsist')
+            
     def create_device(self,group_name,device_type,name):
         if group_name in self.groups:
             topic=f'home/{group_name}/{device_type}/{name}'
@@ -68,6 +76,7 @@ class admin_panel:
             print(f'device {device_type} craeted in group {group_name}')
         else:
             print('your group does not exist')
+            
     def create_multiple_devices(self,group_name,device_type,number_of_devcies):
         if group_name in self.groups:
             for i in range(1,number_of_devcies+1):
@@ -78,22 +87,26 @@ class admin_panel:
             print(f'{number_of_devcies} device {device_type} added to group {group_name}')
         else:
             print(f'group {group_name} does not exist')
+            
     def get_devices_in_groups(self,group_name):
         if group_name in self.groups:
             return self.groups[group_name]
             print(f'all devises in {group_name} ')
         else:
             print(f'group {group_name} does not exist')
+            
     def turn_on_all_in_groups(self,group_name):
         devices=self.get_devices_in_groups(group_name) 
         for device in devices:
             device.turn_on()
         print(f'all devices in group {group_name} are now turned on')
+        
     def turn_off_all_in_groups(self,group_name):
         devises=self.get_devices_in_groups(group_name)
         for devise in devises:
             devise.turn_off()
         print(f'all devices in group {group_name} are now turned off')
+        
     def turn_on_all_devices(self):
        for devices in self.groups.values():
           all_device=[]
@@ -101,6 +114,7 @@ class admin_panel:
        for i in all_device:
            i.turn_on()
        print('all devices are turned on now')
+        
     def turn_off_all_devices(self):
         for devices in self.groups.values():
             all_device=[]
@@ -108,6 +122,7 @@ class admin_panel:
         for i in all_device:
             i.turn_off()
         print('all devices are turned off now')
+        
     def get_status_in_group(self,gorup_name):
         if gorup_name in self.groups:
             all_device=self.get_devices_in_groups(gorup_name)
@@ -118,6 +133,7 @@ class admin_panel:
                     print(f'device {i.name} is off')
         else:
             print(f'group {gorup_name} does not exsist')
+            
     def get_status_in_device_type(self,device_type):
         all_device=[]
         for i in self.groups.values():
@@ -131,12 +147,14 @@ class admin_panel:
           else:
             print(f'{device_type} not found')
             break
+              
     def add_sensor_in_group(self,group_name,sensor):
         if group_name in self.groups:
             self.groups[group_name].append(sensor)
             print(f'sensor {sensor} added in group {group_name}')
         else:
             print('f group {group_name} does not exsist')
+            
     def create_sensor(self,name,group_name,unit):
          if group_name in self.groups:
             topic=f'{name}/{group_name}/{unit}'
@@ -145,6 +163,7 @@ class admin_panel:
             print(f'sensor {name} craeted in group {group_name}')
          else:
            print('your group does not exist') 
+             
     def get_data_from_sensor_in_group(self,group_name):
         if group_name in self.groups:
              for i in self.groups.values():
@@ -152,6 +171,7 @@ class admin_panel:
                  print(f'sensor {i.name} data: {i.read_sensor()} {i.unit}')
         else:
             print(f'group {group_name} does not exsist')
+            
     def delete_all_devives_in_group(self,group_name):
       new_group = []
         if group_name in self.groups:
@@ -162,6 +182,7 @@ class admin_panel:
            print(f'all sensors in group {group_name} deleted')
         else:
            print(f'group {group_name} does not exist')
+            
     def delete_all_sensors_in_group(self, group_name):
       new_group = []
       if group_name in self.groups:
@@ -172,6 +193,7 @@ class admin_panel:
         print(f'all sensors in group {group_name} deleted')
       else:
         print(f'group {group_name} does not exist')
+          
     def rename_groupname(self,group_name,new_group_name):
         if group_name in self.groups:
              if new_group_name not in self.groups:
@@ -182,6 +204,7 @@ class admin_panel:
                  print(f'group {new_group_name} is duplicate')
         else:
             print(f'group {group_name} does not exsist')
+            
     def status_one_device_in_group(self,device_type,name):
         all_device=[]
         for i in self.groups.values():
